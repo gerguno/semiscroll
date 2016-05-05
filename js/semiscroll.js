877df61ce47724ff2820423ef012f4f848327214
admin <gerguno@gmail.com>
2016-05-05 17:07:47 +0300

    improved functionality

--- js/semiscroll.js
+++ js/semiscroll.js
@@ -5,41 +5,39 @@
 
         return this.each(function() {
             var gallery = {},
-                $this = $(this);  //зробити наш елемент видимим у всіх областях видимості ( у "$(window).scroll()" його не видно)
+                $this = $(this),  //зробити наш елемент видимим у всіх областях видимості ( у "$(window).scroll()" його не видно)
+                windowHeight = window.innerHeight;
 
-            gallery.topPosition = $this.offset().top;
-            gallery.bottomPosition = gallery.topPosition + $this.height();
-
+            gallery.topPosition = Math.round($this.offset().top),
+            gallery.bottomPosition = Math.round(gallery.topPosition + $this.height()),
+                
             $this.addClass('pre-scroll');
-
 
             $(window).scroll( function() {
 
                     var scrolled = window.pageYOffset;
-                    console.log('scrolled: ' + scrolled + 'px');
-                    console.log('gallery top: ' + gallery.topPosition + 'px');
-                    console.log('gallery bottom: ' + gallery.bottomPosition + 'px');
-                    console.log('-------------------------');
+                    // console.log('scrolled: ' + scrolled + 'px');
+                    // console.log('gallery top: ' + gallery.topPosition + 'px');
+                    // console.log('gallery bottom: ' + gallery.bottomPosition + 'px');
+                    // console.log('-------------------------');
 
-                        if (gallery.bottomPosition >= scrolled + document.documentElement.clientHeight && scrolled >= gallery.topPosition) {    
-                            $this.addClass('on-scroll');
-                            $this.removeClass('post-scroll');
-                            console.log('On-scroll');
-                        }  
-
-
-
-                        if (gallery.bottomPosition <= scrolled + document.documentElement.clientHeight) {
-                            console.log('Post-scroll');
-                            $this.addClass('post-scroll');
-                        }
-
-
-                        if (scrolled <= gallery.topPosition) {
-                            console.log('Pre-scroll');
+                        if (scrolled < gallery.topPosition) {
+                            // console.log('Pre-scroll');
                             $this.addClass('pre-scroll');
                             $this.removeClass('on-scroll');
                             $this.removeClass('post-scroll');
+                        }
+
+                        if (scrolled > gallery.topPosition && scrolled + windowHeight < gallery.bottomPosition) {    
+                            // console.log('On-scroll');
+                            $this.addClass('on-scroll');
+                            $this.removeClass('post-scroll');
+                        }  
+
+                        if (scrolled + windowHeight > gallery.bottomPosition) {
+                            // console.log('Post-scroll');
+                            $this.removeClass('on-scroll');
+                            $this.addClass('post-scroll');
                         }
             });
         });
